28fe047608d5f3c6923103166b7098f20622b831
Jordi <32176043+jordisf@users.noreply.github.com>
2025-07-16 21:45:47 +0200

    Update raspivan.sh

--- raspivan.sh
+++ raspivan.sh
@@ -2,6 +2,6 @@ sudo apt update
 sudo apt full-upgrade -y
 sudo apt install nodejs npm -y
 
-echo "#### Installed Node and Npm versions ######
+echo "#### Installed Node and Npm versions ######"
 node -v
 npm -v
